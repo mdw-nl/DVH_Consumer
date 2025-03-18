19b54dc2b44d4c9816e2c8105ca09376ee979bcd
Tim Hendriks <tim.hendriks@medicaldataworks.nl>
2025-03-18 13:13:50 +0100

    Add first combination logic

--- DICOM_solver/roi_handler.py
+++ DICOM_solver/roi_handler.py
@@ -1,9 +1,21 @@
 
 from typing import List
 from array import array
+import numpy as np
 from rt_utils import RTStruct
 
-
-def combineRois(contourData: RTStruct, rois: List[str], operators: List[str]) -> array[bool]:
-    return []
-    
+def combine_rois(rtstruct: RTStruct, rois: List[str], operators: List[str]) -> array[bool]:
+    assert rois.__len__() - 1 == operators.__len__(), "There should be exactly one operator less than the number of ROIs in order to combine the ROIs."
+    combined_mask = np.zeros((512, 512, 290), dtype=bool)
+    operatorCount = -1
+    for roi in rois:
+        roiMask = rtstruct.get_roi_mask_by_name(roi)
+        if operatorCount == -1:
+            combinedMask = roiMask
+        if operators[operatorCount] == "+":
+            combined_mask = np.logical_or(combinedMask, roiMask)
+        elif operators[operatorCount] == "-":
+            combined_mask = np.logical_and(combinedMask, np.logical_not(roiMask))
+        operatorCount += 1
+    print(f"Mask for {roi}: {combined_mask}")
+    return combined_mask
--- tests/roi_handler_test.py
+++ tests/roi_handler_test.py
@@ -5,8 +5,11 @@ import os
 import zipfile
 import DICOM_solver.roi_handler as roi_handler
 import urllib.request
+from rt_utils import RTStructBuilder
 
 class TestROIHandler(unittest.TestCase):
+    rtstruct = None
+
     def setUp(self):
         url = 'https://github.com/mdw-nl/test-data/releases/download/dicom-data-1.0.0/dicomdata.zip'
         zip_path = 'dicomtestdata.zip'
@@ -15,10 +18,25 @@ class TestROIHandler(unittest.TestCase):
         zip_path = 'dicomtestdata.zip'
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall('')
+        rtstruct_path = os.path.join("dicomdata","RS.PYTIM05_.dcm")
+        dicom_series_path = os.path.join("dicomdata")
+        self.rtstruct = RTStructBuilder.create_from(dicom_series_path, rtstruct_path)
 
-    def test_combine_rois(self):
-        
-        roi_handler.combineRois([], [], [])
+    def test_combine_rois_addition(self):
+        combined_mask = roi_handler.combine_rois(self.rtstruct, ["Kidney - left_P","Kidney - right_P"], ["+"])
+        print(combined_mask)
+
+    def test_combine_rois_subtraction(self):
+        combined_mask = roi_handler.combine_rois(self.rtstruct, ["Kidney - left_P","GTV_P"], ["-"])
+        print(combined_mask)
+
+    def test_combine_rois_multiple(self):
+        combined_mask = roi_handler.combine_rois(self.rtstruct, ["Kidney - left_P","Kidney - right_P", "GTV_P"], ["+","-"])
+        print(combined_mask)
+
+    def test_combine_rois_invalid(self):
+        with self.assertRaises(AssertionError):
+            roi_handler.combine_rois(self.rtstruct, ["Kidney - left_P","Kidney - right_P"], ["+","-"])
 
 if __name__ == '__main__':
     unittest.main()
