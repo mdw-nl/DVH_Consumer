f8d5de304d874abe64c3c4da3c57606d8069beaf
Tim Hendriks <tim.hendriks@medicaldataworks.nl>
2025-03-18 14:10:30 +0100

    Add constants for tests

--- tests/roi_handler_test.py
+++ tests/roi_handler_test.py
@@ -7,36 +7,43 @@ import DICOM_solver.roi_handler as roi_handler
 import urllib.request
 from rt_utils import RTStructBuilder
 
+ZIP_PATH = 'dicomtestdata.zip'
+DICOM_DATA_PATH = 'dicomdata'
+DICOM_URL = 'https://github.com/mdw-nl/test-data/releases/download/dicom-data-1.0.0/dicomdata.zip'
+RTSTRUCT_FILENAME = "RS.PYTIM05_.dcm"
+ROI_KIDNEY_LEFT = "Kidney - left_P"
+ROI_KIDNEY_RIGHT = "Kidney - right_P"
+ROI_GTV = "GTV_P"
+OPERATION_ADDITION = "+"
+OPERATION_SUBTRACTION = "-"
+
 class TestROIHandler(unittest.TestCase):
     rtstruct = None
 
     def setUp(self):
-        url = 'https://github.com/mdw-nl/test-data/releases/download/dicom-data-1.0.0/dicomdata.zip'
-        zip_path = 'dicomtestdata.zip'
-        if not os.path.exists(zip_path):
-            urllib.request.urlretrieve(url, zip_path)
-        zip_path = 'dicomtestdata.zip'
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        if not os.path.exists(ZIP_PATH):
+            urllib.request.urlretrieve(DICOM_URL, ZIP_PATH)
+        with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref:
             zip_ref.extractall('')
-        rtstruct_path = os.path.join("dicomdata","RS.PYTIM05_.dcm")
-        dicom_series_path = os.path.join("dicomdata")
+        rtstruct_path = os.path.join(DICOM_DATA_PATH, RTSTRUCT_FILENAME)
+        dicom_series_path = os.path.join(DICOM_DATA_PATH)
         self.rtstruct = RTStructBuilder.create_from(dicom_series_path, rtstruct_path)
 
     def test_combine_rois_addition(self):
-        combined_mask = roi_handler.combine_rois(self.rtstruct, ["Kidney - left_P","Kidney - right_P"], ["+"])
+        combined_mask = roi_handler.combine_rois(self.rtstruct, [ROI_KIDNEY_LEFT, ROI_KIDNEY_RIGHT], [OPERATION_ADDITION])
         print(combined_mask)
 
     def test_combine_rois_subtraction(self):
-        combined_mask = roi_handler.combine_rois(self.rtstruct, ["Kidney - left_P","GTV_P"], ["-"])
+        combined_mask = roi_handler.combine_rois(self.rtstruct, [ROI_KIDNEY_LEFT, ROI_GTV], [OPERATION_SUBTRACTION])
         print(combined_mask)
 
     def test_combine_rois_multiple(self):
-        combined_mask = roi_handler.combine_rois(self.rtstruct, ["Kidney - left_P","Kidney - right_P", "GTV_P"], ["+","-"])
+        combined_mask = roi_handler.combine_rois(self.rtstruct, [ROI_KIDNEY_LEFT, ROI_KIDNEY_RIGHT, ROI_GTV], [OPERATION_ADDITION, OPERATION_SUBTRACTION])
         print(combined_mask)
 
     def test_combine_rois_invalid(self):
         with self.assertRaises(AssertionError):
-            roi_handler.combine_rois(self.rtstruct, ["Kidney - left_P","Kidney - right_P"], ["+","-"])
+            roi_handler.combine_rois(self.rtstruct, [ROI_KIDNEY_LEFT, ROI_KIDNEY_RIGHT], [OPERATION_ADDITION, OPERATION_SUBTRACTION])
 
 if __name__ == '__main__':
     unittest.main()
--- tests/roi_lookup_service_test.py
+++ tests/roi_lookup_service_test.py
@@ -3,16 +3,20 @@
 import unittest
 from DICOM_solver.roi_lookup_service import RoiLookupService
 
+ROI_NAME = "LungL"
+STANDARDIZED_NAME = "Lung_L"
+INVALID_ROI_NAME = "InvalidName"
+
 class TestRoiLookupService(unittest.TestCase):
 
     def test_get_roi_name(self):
         roi_lookup_service = RoiLookupService()
-        standardized_name = roi_lookup_service.get_standardized_name("LungL")
-        self.assertEqual(standardized_name, "Lung_L")
+        standardized_name = roi_lookup_service.get_standardized_name(ROI_NAME)
+        self.assertEqual(standardized_name, STANDARDIZED_NAME)
     
     def test_get_roi_name_invalid(self):
         roi_lookup_service = RoiLookupService()
-        standardized_name = roi_lookup_service.get_standardized_name("InvalidName")
+        standardized_name = roi_lookup_service.get_standardized_name(INVALID_ROI_NAME)
         self.assertIsNone(standardized_name)
 
 if __name__ == '__main__':
